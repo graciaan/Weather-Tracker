36b4eb61a8f7e1f07a5f6f6e48ac0be920fa14f9
Andrew <andrewg87@gmail.com>
2023-03-07 20:02:39 -0500

    added more code to script.js

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,29 @@ var cityInput = $(".city-input");
 var searchBtn = $(".btn");
 var searchHistory = $(".history");
 
+var cityArray =JSON.parse(localStorage.getItem('savedCity')) || [];
 
+function savedCity() {
+  event.preventDefault();
 
+  cityInput = $('#city-input').val();
+  cityArray.push(cityInput);
 
+  localStorage.setItem('savedCity', JSON.stringify(cityArray));
+
+  searchHistory.empty();
+
+  displayCities();
+  getCurrentWeather();
+}
+
+function displayCities() {
+  for (var i = 0; i < cityArray.length; i++) {
+    var cityList = $('<li>').addClass('list-group-item').text(cityArray[i]);
+    searchHistory.append(cityList);
+  }
+}
+
+displayCities();
+searchBtn.click(savedCity);
 
